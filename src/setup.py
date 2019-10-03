e2fffc0e269e2ecfb5b4b0014dad3adaa0771d36
Damon McCullough <damonmcc1391@gmail.com>
2019-10-03 16:51:13 -0400

    Added __init__.py's and setup.py

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='kairosight',
+    version='',
+    packages=['tests', 'algorithms'],
+    package_dir={'': 'src'},
+    url='https://github.com/kairosight/kairosight',
+    license='GNU General Public License v3.0',
+    author='dmcculloug',
+    author_email='',
+    description='Analysis of scientific images across time'
+)
