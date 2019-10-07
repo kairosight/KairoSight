d4e3a69b3bff64b84925444ab7e37a285370fd5b
Damon McCullough <damonmcc1391@gmail.com>
2019-10-07 16:07:46 -0400

    Renamed model name, added calcium model

--- src/algorithms/datamodel.py
+++ src/algorithms/datamodel.py
@@ -2,22 +2,27 @@ from math import pi, floor
 import numpy as np
 
 
-def model_vm(t=500, t0=50, fps=1000, f0=0, f_peak=10):
-    """Create an array of model 16-bit optical data of the murine action potential (OAP).
+def model_transient(model_type='Vm', t=100, t0=0, fps=1000, f0=100, f_amp=10, noise=0):
+    """Create an array of model 16-bit optical data of either a murine action potential (OAP)
+    or a murine calcium transient (OCT).
 
        Parameters
        ----------
+       model_type : str
+           The type of transient: 'Vm' or 'Ca', default is 'Vm'
        t : int, float
-           Length of array in milliseconds (ms)
+           Length of array in milliseconds (ms), default is 100
        t0 : int, float
-           Start time (ms) of first OAP
+           Start time (ms) of first OAP, default is 0
        fps : int
-           Frame rate (frames per second) of optical data acquisition
+           Frame rate (frames per second) of optical data acquisition, default is 1000
        f0 : int
-           Baseline fluorescence value in counts, default is 0
-       f_peak : int
+           Baseline fluorescence value in counts, default is 100
+       f_amp : int
            Amplitude of the OAP in counts, default is 10.
            Can be negative, e.g. cell depolarization with fast voltage dyes
+       noise : int
+           Magnitude of gaussian noise, as a percentage of f_peak, default is 0
 
        Returns
        -------
@@ -27,18 +32,21 @@ def model_vm(t=500, t0=50, fps=1000, f0=0, f_peak=10):
            An array of model 16-bit OAP data
        """
     # Check parameters
+    if model_type not in ['Vm', 'Ca']:
+        raise ValueError("The model type must either be 'Vm' or 'Ca'")
     if (type(t) or type(t0)) not in [int, float]:
         raise TypeError('All time parameters must be a non-negative real number')
-    if (type(fps) or type(f0) or type(f_peak)) not in [int]:
+    if (type(fps) or type(f0) or type(f_amp)) not in [int]:
         raise TypeError('All fps and fluorescent parameters must be ints')
 
     if t < 100:
         raise ValueError("The time length (t) must be longer than 100 ms ")
     if t0 >= t:
         raise ValueError("The start time (t0) must be less than the time length (t)")
-
     if fps <= 200 or fps > 1000:
         raise ValueError("The fps must be >200 and <1000")
+    if f_amp < 0:
+        raise ValueError("The amplitude must >=0 ")
 
     # Calculate important constants
     FPMS = fps / 1000
@@ -50,7 +58,7 @@ def model_vm(t=500, t0=50, fps=1000, f0=0, f_peak=10):
     FL_COUNT_MAX = 2**16 - 1
     if f0 > FL_COUNT_MAX:
         raise ValueError("The baseline fluorescence (f0) must be less than 2^16 - 1 (65535)")
-    if f_peak > FL_COUNT_MAX:
+    if abs(f_amp) > FL_COUNT_MAX:
         raise ValueError("The OAP amplitude (f_peak) must be less than 2^16 - 1 (65535)")
 
     # Initialize full model arrays
@@ -59,26 +67,66 @@ def model_vm(t=500, t0=50, fps=1000, f0=0, f_peak=10):
     if not np.equal(model_time.size, model_data.size):
         raise ArithmeticError("Lengths of time and data arrays not equal!")
 
-    # Initialize a single OAP array
-    # Depolarization phase
-    model_dep_t = round(10 / FRAME_T)   # 10 ms long
-    model_dep = np.full(model_dep_t, f0)
-    for i in range(0, model_dep_t):
-        model_dep[i] = f0 + (f_peak * np.exp(-((i - model_dep_t)**2)))
-    # Early repolarization phase (20%)
-    model_rep1_t = round(5 / FRAME_T)   # 5 ms long
-    m_rep1 = - (f_peak - (f_peak * 0.8)) / 5     # slope of this phase
-    model_rep1 = np.full(model_rep1_t, f0)
-    for i in range(0, model_rep1_t):
-        model_rep1[i] = ((m_rep1 * i) + f_peak + f0)
-    # Late repolarization phase
-    model_rep2_t = round(25 / FRAME_T)   # 25 ms long
-    model_rep2 = np.full(model_rep2_t, f0 + int((f_peak / 3)))
-    model_oap = np.concatenate((model_dep, model_rep1, model_rep2), axis=None)
+    if model_type is 'Vm':
+        # Initialize a single OAP array (50 ms)
+        vm_amp = -f_amp
+        # Depolarization phase
+        model_dep_period = 10  # 10 ms long
+        model_dep_frames = round(model_dep_period / FRAME_T)
+        model_dep = np.full(model_dep_frames, f0)
+        for i in range(0, model_dep_frames):
+            model_dep[i] = f0 + (vm_amp * np.exp(-(((i - model_dep_frames) / 3) ** 2)))  # a simplified Gaussian function
 
+        # Early repolarization phase (from peak to APD 20, aka 80% of peak)
+        model_rep1_period = 3  # 3 ms long
+        model_rep1_frames = round(model_rep1_period / FRAME_T)
+        m_rep1 = -(vm_amp - (vm_amp * 0.8)) / model_rep1_period     # slope of this phase
+        model_rep1 = np.full(model_rep1_frames, f0)
+        for i in range(0, model_rep1_frames):
+            model_rep1[i] = ((m_rep1 * i) + vm_amp + f0)    # linear
+
+        # Late repolarization phase
+        model_rep2_period = 50 - model_dep_period - model_rep1_period  # remaining OAP time
+        model_rep2_frames = round(model_rep2_period / FRAME_T)
+        model_rep2_t = np.linspace(0, model_rep2_period, model_rep2_frames)
+        A, B, C = vm_amp * 0.8, (2 / m_rep1), f0     # exponential decay parameters
+        model_rep2 = A * np.exp(-B * model_rep2_t) + C    # exponential decay, concave down
+        model_rep2 = model_rep2.astype(int, copy=False)
+
+    else:
+        # Initialize a single OCT array (100 ms)
+        # Depolarization phase
+        model_dep_period = 15  # XX ms long
+        model_dep_frames = round(model_dep_period / FRAME_T)
+        model_dep = np.full(model_dep_frames, f0)
+        for i in range(0, model_dep_frames):
+            model_dep[i] = f0 + (f_amp * np.exp(-(((i - model_dep_frames) / 6) ** 2)))  # a simplified Gaussian function
+
+        # Early repolarization phase (from peak to CAD 40, aka 60% of peak)
+        model_rep1_period = 15  # XX ms long
+        cad_ratio = 0.6
+        model_rep1_frames = round(model_rep1_period / FRAME_T)
+        m_rep1 = -(f_amp - (f_amp * cad_ratio)) / model_rep1_period     # slope of this phase
+        model_rep1 = np.full(model_rep1_frames, f0)
+        for i in range(0, model_rep1_frames):
+            model_rep1[i] = ((m_rep1 * i) + f_amp + f0)    # linear
+
+        # Late repolarization phase
+        model_rep2_period = 100 - model_dep_period - model_rep1_period  # remaining OAP time
+        model_rep2_frames = round(model_rep2_period / FRAME_T)
+        model_rep2_t = np.linspace(0, model_rep2_period, model_rep2_frames)
+        A, B, C = f_amp * cad_ratio, (0.3 / m_rep1), f0     # exponential decay parameters
+        model_rep2 = A * np.exp(B * model_rep2_t) + C    # exponential decay, concave up
+        model_rep2 = model_rep2.astype(int, copy=False)
+
+    # Assemble the transient
+    model_tran = np.concatenate((model_dep, model_rep1, model_rep2), axis=None)
     # Assemble the start time and single OAP into the full array
+    model_data[FRAME_T0:FRAME_T0 + model_tran.size] = model_tran
 
-    model_data[FRAME_T0:FRAME_T0 + model_oap.size] = model_oap
+    # Add gaussian noise, mean: 0, standard deviation: 10% of peak, length
+    model_noise = np.random.normal(0, (noise/100) * f_amp, model_data.size)
+    model_data = model_data + model_noise
 
     return model_time, model_data
 
--- src/tests/test_datamodel.py
+++ src/tests/test_datamodel.py
@@ -1,45 +1,72 @@
 import unittest
-from algorithms.datamodel import model_vm, circle_area
+from algorithms.datamodel import model_transient, circle_area
 from math import pi
+import matplotlib.pyplot as plt
+import matplotlib.ticker as plticker
+fontsize1, fontsize2, fontsize3, fontsize4 = [14, 10, 8, 6]
 
 
-class TestModelVm(unittest.TestCase):
+class TestModelTransient(unittest.TestCase):
     def test_model_params(self):
         # Make sure parameters are valid, and valid errors are raised when necessary
-        self.assertRaises(ValueError, model_vm, t=-2)     # no negative total times
-        self.assertRaises(ValueError, model_vm, t=99)     # at least 100 ms long
-        self.assertRaises(ValueError, model_vm, t=150, t0=150)       # start time must be lower than the total time
-        self.assertRaises(ValueError, model_vm, t=100, t0=0, fps=150)      # no fps < 200
-        self.assertRaises(ValueError, model_vm, t=100, t0=0, fps=1001)      # no fps > 1000
-        self.assertRaises(ValueError, model_vm, t=100, t0=0, fps=500, f0=2**16)     # no baseline > 16-bit max
-        self.assertRaises(ValueError, model_vm, t=100, t0=0, fps=500,  f0=0, f_peak=2**16)  # no amplitude > 16-bit max
+        self.assertRaises(ValueError, model_transient, t=-2)     # no negative total times
+        self.assertRaises(ValueError, model_transient, t=99)     # at least 100 ms long
+        self.assertRaises(ValueError, model_transient, t=150, t0=150)       # start time < than total time
+        self.assertRaises(ValueError, model_transient, t=100, t0=0, fps=150)      # no fps < 200
+        self.assertRaises(ValueError, model_transient, t=100, t0=0, fps=1001)      # no fps > 1000
+        self.assertRaises(ValueError, model_transient, t=100, t0=0, fps=500, f0=2**16)     # no baseline > 16-bit max
+        self.assertRaises(ValueError, model_transient, t=100, t0=0, fps=500,  f0=0, f_amp=2**16)  # no amplitude > 16-bit max
+        self.assertRaises(ValueError, model_transient, t=100, t0=0, fps=500,  f0=0, f_amp=-2)   # no amplitude < 0
 
         # Make sure type errors are raised when necessary
-        self.assertRaises(TypeError, model_vm, t=3+5j)
-        self.assertRaises(TypeError, model_vm, t=True)
-        self.assertRaises(TypeError, model_vm, t='radius')
+        self.assertRaises(TypeError, model_transient, t=3+5j)
+        self.assertRaises(TypeError, model_transient, t=True)
+        self.assertRaises(TypeError, model_transient, t='radius')
         # Some parameters must be int
-        self.assertRaises(TypeError, model_vm, t=250, t0=20, fps=50.3, f0=1.5, f_peak=10.4)
+        self.assertRaises(TypeError, model_transient, t=250, t0=20, fps=50.3, f0=1.5, f_amp=10.4)
 
     def test_model_result(self):
         # Make sure model results are valid
-        self.assertEqual(len(model_vm(t=150)), 2)      # time and data arrays returned as a tuple
-        self.assertEqual(model_vm(1000)[0].size, model_vm(1000)[1].size)    # time and data arrays are same length
+        self.assertEqual(len(model_transient(t=150)), 2)      # time and data arrays returned as a tuple
+        self.assertEqual(model_transient(t=1000)[0].size, model_transient(t=1000)[1].size)    # time and data same size
         # Test the returned time array
-        self.assertEqual(model_vm(t=150)[0].size, 150)      # length is correct
-        self.assertEqual(model_vm(t=1000, t0=10, fps=223)[0].size, 223)      # length is correct with odd fps
-        self.assertGreaterEqual(model_vm(100)[0].all(), 0)     # no negative times
-        self.assertLess(model_vm(200)[0].all(), 200)     # no times >= total time parameter
+        self.assertEqual(model_transient(t=150)[0].size, 150)      # length is correct
+        self.assertEqual(model_transient(t=1000, t0=10, fps=223)[0].size, 223)      # length is correct with odd fps
+        self.assertGreaterEqual(model_transient(t=100)[0].all(), 0)     # no negative times
+        self.assertLess(model_transient(t=200)[0].all(), 200)     # no times >= total time parameter
         # Test the returned data array
-        self.assertEqual(model_vm(t=150)[1].size, 150)      # length is correct
-        self.assertEqual(model_vm(t=1000, t0=10, fps=223)[1].size, 223)     # length is correct with odd fps
-        self.assertGreaterEqual(model_vm(t=100)[1].all(), 0)     # no negative values
-        self.assertLess(model_vm(t=100)[1].all(), 2**16)     # no values >= 16-bit max
+        self.assertEqual(model_transient(t=150)[1].size, 150)      # length is correct
+        self.assertEqual(model_transient(t=1000, t0=10, fps=223)[1].size, 223)     # length is correct with odd fps
+        self.assertGreaterEqual(model_transient(t=100)[1].all(), 0)     # no negative values
+        self.assertLess(model_transient(t=100)[1].all(), 2**16)     # no values >= 16-bit max
+        self.assertGreaterEqual(2000, model_transient(model_type='Vm', f0=2000)[1].max())  # Vm amplitude handled properly
+        self.assertLessEqual(2000, model_transient(model_type='Ca', f0=2000)[1].min())  # Ca amplitude handled properly
 
     def test_model_ideal(self):
         # Test ideal model Vm data
-        time, data = model_vm(t=500, t0=50, fps=1000, f0=2000, f_peak=250)
-        self.assertEqual(time.size, data.size)     # data and time arrays returned as a tuple
+        time_vm, data_vm = model_transient(model_type='Vm', f0=2000, f_amp=250, noise=5)
+        time_ca, data_ca = model_transient(model_type='Ca', f0=1000, f_amp=250, noise=5)
+        self.assertEqual(time_vm.size, data_vm.size)     # data and time arrays returned as a tuple
+
+        # Build a figure to plot model data
+        fig = plt.figure(figsize=(8, 5))  # _ x _ inch page
+        plt.rc('xtick', labelsize=fontsize2)
+        plt.rc('ytick', labelsize=fontsize2)
+        ax = fig.add_subplot(111)
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        ax.tick_params(axis='x', which='minor', length=3, bottom=True, top=True)
+        ax.tick_params(axis='x', which='major', length=8, bottom=True, top=False)
+        ax.xaxis.set_major_locator(plticker.MultipleLocator(25))
+        ax.xaxis.set_minor_locator(plticker.MultipleLocator(5))
+        # ax.set_ylim([1500, 2500])
+
+        # Plot aligned model data
+        plot_vm, = ax.plot(time_vm, -(data_vm-2000), 'r+', label='Vm')
+        plot_ca, = ax.plot(time_ca, data_ca-1000, 'y+', label='Ca')
+        plot_baseline = ax.axhline(color='gray', linestyle='--', label='baseline')
+
+        fig.show()
 
 
 # Example tests
