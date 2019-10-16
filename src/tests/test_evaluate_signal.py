7dd68cf174219bbbc467335a84a1ba36e77bf88c
Nina Ciccarelli <ninaciccarelli@users.noreply.github.com>
2019-10-16 14:04:07 -0400

    updated

--- src/tests/test_evaluate_signal.py
+++ src/tests/test_evaluate_signal.py
@@ -8,18 +8,20 @@ class TestEvaluateSNR(unittest.TestCase):
     def test_params(self):
         # Make sure type errors are raised when necessary
         self.assertRaises(TypeError, calculate_snr, signal=True, i_noise=(0, 10), i_peak=(40, 50))
-        self.assertRaises(TypeError, calculate_snr, signal=True, i_noise=(0, 10), i_peak=(60, 70))
-
-        # Make sure parameters are valid, and valid errors are raised when necessary
-        self.assertRaises(ValueError, calculate_snr, signal=True, i_noise=(0, 10), i_peak=(40, 50))
-        self.assertRaises(ValueError, calculate_snr, signal=True, i_noise=(0, 10), i_peak=(60, 70))
-
-        # Make sure type errors are raised when necessary
-        self.assertRaises(TypeError, calculate_snr, signal=True, t_noise=(0, 10), t_peak=(50, 60))
-        self.assertRaises(TypeError, calculate_snr, signal=True)
+        self.assertRaises(TypeError, calculate_snr, signal=3+5j, i_noise=(0, 10), i_peak=(40, 50))
+        self.assertRaises(TypeError, calculate_snr, signal='radius', i_noise=(0, 10), i_peak=(40, 50))
+        self.assertRaises(TypeError, calculate_snr, i_peak='radius')
+        self.assertRaises(TypeError, calculate_snr, i_peak=True)
+        self.assertRaises(TypeError, calculate_snr, i_peak=3+5j)
         self.assertRaises(TypeError, calculate_snr, t='radius')
         self.assertRaises(TypeError, calculate_snr, t=True)
         self.assertRaises(TypeError, calculate_snr, t=3+5j)
+
+        time_ca, signal = model_transients(model_type='Ca', f_0=1000, f_amp=250, noise=5)
+
+        # Make sure parameters are valid, and valid errors are raised when necessary
+        self.assertRaises(ValueError, calculate_snr, signal=True, i_noise=(0, 10), i_peak=(40, 50))
+        self.assertRaises(ValueError, calculate_snr, signal, i_noise=(0, 15), i_peak=(40, 45)) # range used should be 10
 
     def test_results(self):
         time_ca, signal = model_transients(model_type='Ca', f_0=1000, f_amp=250, noise=5)
@@ -41,15 +43,21 @@ class TestEvaluateError(unittest.TestCase):
         self.assertRaises(TypeError, calculate_error, ideal=3+5j, modified='radius')
 
         # Make sure parameters are valid, and valid errors are raised when necessary
-        self.assertRaises(ValueError, calculate_error, ideal=-2, modified=-2)
-        self.assertRaises(ValueError, calculate_error, ideal=1.3, modified=1.3)
+        self.assertRaises(ValueError, calculate_error, ideal=-2, modified=-2)  # values should be positive
+        self.assertRaises(ValueError, calculate_error, ideal=1.3, modified=1.3)  # ideal and modified should be ints
 
     def test_results(self):
-        time_ca, ideal, modified = model_transients(model_type='Ca', f_0=1000, f_amp=250, noise=5)
+        time_ca, ideal = model_transients(model_type='Ca', f_0=1000, f_amp=250, noise=5)
+        time_ca, modified = model_transients(model_type='Ca', f_0=1000, f_amp=250, noise=5)
         # Make sure files are opened and read correctly
-        self.assertIsInstance(calculate_error(ideal, modified, [0]), np.ndarray)  # array of error
-        self.assertIsInstance(calculate_error(ideal, modified, [1]), float)  # mean value of error array
-        self.assertIsInstance(calculate_error(ideal, modified, [2]), float)  # sd of error array
+        self.assertIsInstance(calculate_error(ideal, modified)[0], np.ndarray)  # array of error
+        self.assertIsInstance(calculate_error(ideal, modified)[1], float)  # mean value of error array
+        self.assertIsInstance(calculate_error(ideal, modified)[2], float)  # sd of error array
+        # Test returned error array  
+        self.assertGreaterEqual(calculate_error(ideal, modified)[0].all, 0)  # no negative values
+        self.assertEqual(calculate_error(ideal, modified)[1].size, calculate_error(ideal, modified)[2].size) # ideal and modified the same size
+
+        #  self.assertEqual(calculate_error(ideal, modified)[1].mean())  # mean of percent array found properly
 
 if __name__ == '__main__':
     unittest.main()
