af0b041044a95e33fd51a14767cad801527153ea
Damon McCullough <damonmcc1391@gmail.com>
2019-10-16 16:39:08 -0400

    Added /tests/data/ and 3 files for TestPrepOpen

--- src/tests/test_preparation.py
+++ src/tests/test_preparation.py
@@ -6,14 +6,15 @@ from pathlib import Path
 
 
 class TestPrepOpen(unittest.TestCase):
+    # File paths  and files needed for tests
     cwd = Path.cwd()
     tests = str(cwd)
-
     file_single1 = tests + '/data/about1.tif'
-    file_single2 = tests + '/data/02-250.tif'
-    file_meta = tests + '/data/02-250.pcoraw.rec'
-    file_single1_wrong = tests + '/data/02-250_Vm'
-    file_meta_wrong = tests + '/data/02-250_Vm.pcoraw'
+    file_single1_wrong = tests + '/data/about1'
+    file_single2 = tests + '/data/02-250_Vm.tif'
+    file_single2_wrong = tests + '/data/02-250_Vm'
+    file_meta = tests + '/data/02-250_Vm.pcoraw.rec'
+    file_meta_wrong = tests + '/data/02-250.pcoraw.txt'
     print("sys.maxsize : " + str(sys.maxsize) +
           ' \nIs it greater than 32-bit limit? : ' + str(sys.maxsize > 2 ** 32))
 
