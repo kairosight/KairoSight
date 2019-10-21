33e74197a5d9f0bd99978c1c33c8f40986147656
Damon McCullough <damonmcc1391@gmail.com>
2019-10-21 16:44:07 -0400

    Correcting model data for different fps

--- src/util/datamodel.py
+++ src/util/datamodel.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def model_transients(model_type='Vm', t=100, t0=0, fps=1000, f_0=100, f_amp=10, noise=0, num=1):
-    """Create an array of model 16-bit optical data of either a
+    """Create a 2-D array of model 16-bit optical data of either a
     murine action potential (OAP) or a murine calcium transient (OCT).
 
        Parameters
@@ -15,7 +15,7 @@ def model_transients(model_type='Vm', t=100, t0=0, fps=1000, f_0=100, f_amp=10, 
        t0 : int, float
            Start time (ms) of first OAP, default is 0
        fps : int
-           Frame rate (frames per second) of optical data acquisition, default is 1000
+           Frame rate (frames per second) of optical data acquisition, default is 1000, min is 200
        f_0 : int
            Baseline fluorescence value in counts, default is 100
        f_amp : int
@@ -85,53 +85,66 @@ def model_transients(model_type='Vm', t=100, t0=0, fps=1000, f_0=100, f_amp=10, 
         vm_amp = -f_amp
         # Depolarization phase
         model_dep_period = 10  # 10 ms long
-        model_dep_frames = round(model_dep_period / FRAME_T)
+        model_dep_frames = floor(model_dep_period / FRAME_T)
         model_dep = np.full(model_dep_frames, f_0)
         for i in range(0, model_dep_frames):
             model_dep[i] = f_0 + (vm_amp * np.exp(-(((i - model_dep_frames) / 3) ** 2)))  # a simplified Gaussian function
 
+
+
         # Early repolarization phase (from peak to APD 20, aka 80% of peak)
-        model_rep1_period = 3  # 3 ms long
-        model_rep1_frames = round(model_rep1_period / FRAME_T)
-        m_rep1 = -(vm_amp - (vm_amp * 0.8)) / model_rep1_period     # slope of this phase
+        model_rep1_period = 5  # 5 ms long
+        model_rep1_frames = floor(model_rep1_period / FRAME_T)
+        apd_ratio = 0.8
+        m_rep1 = -(vm_amp - (vm_amp * apd_ratio)) / model_rep1_period     # slope of this phase
         model_rep1 = np.full(model_rep1_frames, f_0)
         for i in range(0, model_rep1_frames):
             model_rep1[i] = ((m_rep1 * i) + vm_amp + f_0)    # linear
 
         # Late repolarization phase
         model_rep2_period = 50 - model_dep_period - model_rep1_period  # remaining OAP time
-        model_rep2_frames = round(model_rep2_period / FRAME_T)
+        model_rep2_frames = floor(model_rep2_period / FRAME_T)
         model_rep2_t = np.linspace(0, model_rep2_period, model_rep2_frames)
-        A, B, C = vm_amp * 0.8, (2 / m_rep1), f_0     # exponential decay parameters
+        A, B, C = vm_amp * 0.8, (5 / m_rep1), f_0     # exponential decay parameters
         model_rep2 = A * np.exp(-B * model_rep2_t) + C    # exponential decay, concave down
         model_rep2 = model_rep2.astype(int, copy=False)
         # Pad end with 50 ms of baseline
-        model_rep2 = np.concatenate((model_rep2, np.full(50, f_0, dtype=np.int)), axis=None)
+        model_rep2Pad_frames = floor(50 / FRAME_T)
+        model_rep2Pad = np.full(model_rep2Pad_frames, f_0, dtype=np.int)
+        model_rep2 = np.concatenate((model_rep2, model_rep2Pad), axis=None)
 
     else:
         # With calcium dyes, depolarization transients have a positive deflection and return to baseline
         # Initialize a single OCT array (100 ms)
         # Depolarization phase
         model_dep_period = 15  # XX ms long
-        model_dep_frames = round(model_dep_period / FRAME_T)
+        model_dep_frames = floor(model_dep_period / FRAME_T)
         model_dep = np.full(model_dep_frames, f_0)
-        for i in range(0, model_dep_frames):
-            model_dep[i] = f_0 + (f_amp * np.exp(-(((i - model_dep_frames) / 6) ** 2)))  # a simplified Gaussian function
+        model_dep_full = np.full(model_dep_period, f_0)
+        # Generate high-fidelity data
+        for i in range(0, model_dep_period):
+            model_dep_full[i] = f_0 + (f_amp * np.exp(-(((i - model_dep_period) / 6) ** 2)))  # a simplified Gaussian function
+        # Under-sample the high-fidelity data
+        model_dep = model_dep_full[::floor(model_dep_period/model_dep_frames)][:model_dep_frames]
 
         # Early repolarization phase (from peak to CAD 40, aka 60% of peak)
         model_rep1_period = 15  # XX ms long
+        model_rep1_frames = floor(model_rep1_period / FRAME_T)
         cad_ratio = 0.6
-        model_rep1_frames = round(model_rep1_period / FRAME_T)
         m_rep1 = -(f_amp - (f_amp * cad_ratio)) / model_rep1_period     # slope of this phase
         model_rep1 = np.full(model_rep1_frames, f_0)
-        for i in range(0, model_rep1_frames):
-            model_rep1[i] = ((m_rep1 * i) + f_amp + f_0)    # linear
+        model_rep1_full = np.full(model_rep1_period, f_0)
+        # Generate high-fidelity data
+        for i in range(0, model_rep1_period):
+            model_rep1_full[i] = ((m_rep1 * i) + f_amp + f_0)    # linear
+        # Under-sample the high-fidelity data
+        model_rep1 = model_rep1_full[::floor(model_rep1_period/model_rep1_frames)][:model_rep1_frames]
 
         # Late repolarization phase
         model_rep2_period = 100 - model_dep_period - model_rep1_period  # remaining OAP time
-        model_rep2_frames = round(model_rep2_period / FRAME_T)
+        model_rep2_frames = floor(model_rep2_period / FRAME_T)
         model_rep2_t = np.linspace(0, model_rep2_period, model_rep2_frames)
-        A, B, C = f_amp * cad_ratio, (0.3 / m_rep1), f_0     # exponential decay parameters
+        A, B, C = f_amp * cad_ratio, (0.8 / m_rep1), f_0     # exponential decay parameters
         model_rep2 = A * np.exp(B * model_rep2_t) + C    # exponential decay, concave up
         model_rep2 = model_rep2.astype(int, copy=False)
 
@@ -145,6 +158,31 @@ def model_transients(model_type='Vm', t=100, t0=0, fps=1000, f_0=100, f_amp=10, 
     model_data = model_data + model_noise
 
     return model_time, model_data
+
+
+def model_images(model_type='Vm', t=100, noise=0, num=1):
+    """Create a 3-D array of model 16-bit optical data of either a
+    murine action potential (OAP) or a murine calcium transient (OCT).
+
+       Parameters
+       ----------
+       model_type : str
+           The type of transient: 'Vm' or 'Ca', default is 'Vm'
+       t : int, float
+           Length of array in milliseconds (ms), default is 100
+       noise : int
+           Magnitude of gaussian noise, as a percentage of f_peak, default is 0
+       num : int
+           Number of transients to generate, default is 1
+
+       Returns
+       -------
+       model_time : ndarray
+           An array of timestamps corresponding to model_data
+       model_data : ndarray
+           An array of model 16-bit OAP data
+       """
+    # Constants
 
 
 # Code for example tests
