5f8e184c42fa2785697b54d3de8a24e62eb2d8e8
Damon McCullough <damonmcc1391@gmail.com>
2019-11-01 15:51:16 -0400

    Doc formatting

--- src/util/processing.py
+++ src/util/processing.py
@@ -6,17 +6,17 @@ from scipy.signal import find_peaks
 def isolate_spatial(stack_in, roi):
     """Isolate a spatial region of a stack (3-D array, TYX) of grayscale optical data.
 
-       Parameters
-       ----------
-       stack_in : ndarray
-            A 3-D array (T, Y, X) of optical data
-       roi : `GraphicsItem <pyqtgraph.graphicsItems.ROI>`
-            Generic region-of-interest widget.
+        Parameters
+        ----------
+        stack_in : ndarray
+             A 3-D array (T, Y, X) of optical data
+        roi : `GraphicsItem <pyqtgraph.graphicsItems.ROI>`
+             Generic region-of-interest widget.
 
-       Returns
-       -------
-       stack_out : ndarray
-            A spatially isolated 3-D array (T, Y, X) of optical data
+        Returns
+        -------
+        stack_out : ndarray
+             A spatially isolated 3-D array (T, Y, X) of optical data
        """
     pass
 
@@ -24,110 +24,110 @@ def isolate_spatial(stack_in, roi):
 def isolate_temporal(stack_in, i_start, i_end):
     """Isolate a spatial region of a stack (3-D array, TYX) of grayscale optical data.
 
-       Parameters
-       ----------
-       stack_in : ndarray
-            A 3-D array (T, Y, X) of optical data
-       i_start : int
-            Index or frame to start temporal isolation
-       i_end : int
-            Index or frame to end temporal isolation
+        Parameters
+        ----------
+        stack_in : ndarray
+             A 3-D array (T, Y, X) of optical data
+        i_start : int
+             Index or frame to start temporal isolation
+        i_end : int
+             Index or frame to end temporal isolation
 
-       Returns
-       -------
-       stack_out : ndarray
-            A temporally isolated 3-D array (T, Y, X) of optical data
-       """
+        Returns
+        -------
+        stack_out : ndarray
+             A temporally isolated 3-D array (T, Y, X) of optical data
+        """
     pass
 
 
 def isolate_transient(signal_in, i_start, i_end):
     """Isolate a single transient from a signal array of optical data.
 
-       Parameters
-       ----------
-       signal_in : ndarray
-            The array of data to be evaluated
-       i_start : int
-            Index or frame to start transient isolation
-       i_end : int
-            Index or frame to end transient isolation
+        Parameters
+        ----------
+        signal_in : ndarray
+             The array of data to be evaluated
+        i_start : int
+             Index or frame to start transient isolation
+        i_end : int
+             Index or frame to end transient isolation
 
-       Returns
-       -------
-       transient_out : ndarray
-            The isolated array of transient data
-       """
+        Returns
+        -------
+        transient_out : ndarray
+             The isolated array of transient data
+        """
     pass
 
 
 def filter_spatial(stack_in, filter_type):
     """Spatially filter a stack (3-D array, TYX) of grayscale optical data.
 
-       Parameters
-       ----------
-       stack_in : ndarray
-            A 3-D array (T, Y, X) of optical data
-       filter_type : str
-           The type of filter algorithm to use
+        Parameters
+        ----------
+        stack_in : ndarray
+             A 3-D array (T, Y, X) of optical data
+        filter_type : str
+            The type of filter algorithm to use
 
-       Returns
-       -------
-       stack_out : ndarray
-            A spatially filtered 3-D array (T, Y, X) of optical data
-       """
+        Returns
+        -------
+        stack_out : ndarray
+             A spatially filtered 3-D array (T, Y, X) of optical data
+        """
     pass
 
 
 def filter_temporal(signal_in, filter_type):
     """Temporally filter an array of optical data.
 
-       Parameters
-       ----------
-       signal_in : ndarray
-            The array of data to be evaluated
-       filter_type : str
-           The type of filter algorithm to use
+        Parameters
+        ----------
+        signal_in : ndarray
+             The array of data to be evaluated
+        filter_type : str
+            The type of filter algorithm to use
 
-       Returns
-       -------
-       stack_out : ndarray
-            A temporally filtered 3-D array (T, Y, X) of optical data
-       """
+        Returns
+        -------
+        stack_out : ndarray
+             A temporally filtered 3-D array (T, Y, X) of optical data
+        """
     pass
 
 
 def filter_drift(signal_in, poly_order):
     """Remove drift from an array of optical data using a polynomial fit.
 
-       Parameters
-       ----------
-       signal_in : ndarray
-            The array of data to be evaluated
-       poly_order : int
-            The order of the polynomial to fit to
+        Parameters
+        ----------
+        signal_in : ndarray
+             The array of data to be evaluated
+        poly_order : int
+             The order of the polynomial to fit to
 
-       Returns
-       -------
-       signal_out : ndarray
-            A signal array with drift removed
-       """
+        Returns
+        -------
+        signal_out : ndarray
+             A signal array with drift removed
+        """
     pass
 
 
 def invert_signal(signal_in):
     """Invert the values of a signal array.
 
-       Parameters
-       ----------
-       signal_in : ndarray
-            The array of data to be processed
+        Parameters
+        ----------
+        signal_in : ndarray
+             The array of data to be processed
 
-       Returns
-       -------
-       signal_inv : ndarray
-            The inverted signal array
-       """
+        Returns
+        -------
+        signal_inv : ndarray
+             The inverted signal array
+        """
     # Check parameters
     if type(signal_in) is not np.ndarray:
         raise TypeError('Signal data type must be an "ndarray"')
@@ -151,16 +151,16 @@ def invert_signal(signal_in):
 def normalize_signal(signal_in):
     """Normalize the values of a signal array to range from 0 to 1.
 
-       Parameters
-       ----------
-       signal_in : ndarray
-            The array of data to be processed
+        Parameters
+        ----------
+        signal_in : ndarray
+             The array of data to be processed
 
-       Returns
-       -------
-       signal_norm : ndarray
-            The normalized signal array, dtype : float
-       """
+        Returns
+        -------
+        signal_norm : ndarray
+             The normalized signal array, dtype : float
+        """
     # Check parameters
     if type(signal_in) is not np.ndarray:
         raise TypeError('Signal data type must be an "ndarray"')
@@ -181,36 +181,36 @@ def calculate_snr(signal_in, noise_count=10):
     """Calculate the Signal-to-Noise ratio of a signal array,
     defined as the ratio of the Peak-Peak amplitude to the sample standard deviation of the noise.
 
-       Parameters
-       ----------
-       signal_in : ndarray
-            The array of data to be evaluated, dtyoe : int or float
-       noise_count : int
-            The number of noise values to be used in the calculation, default is 10
+        Parameters
+        ----------
+        signal_in : ndarray
+             The array of data to be evaluated, dtyoe : int or float
+        noise_count : int
+             The number of noise values to be used in the calculation, default is 10
 
-       Returns
-       -------
-       snr : float
-            The Signal-to-Noise ratio of the given data
-       rms_bounds : tuple
-            The RMSs of the peak and noise arrays, (noise_rms, peak_rms)
-       peak_peak : float
-            The absolute difference between the RMSs of the peak and noise arrays
-       ratio_noise : float
-            The ratio between the ranges of noise and peak value(s)
-       sd_noise : float
-            The standard deviation of the noise values
-       ir_noise : ndarray
-            The indexes of noise values used in the calculation
-       ir_peak : int
-            The index of peak values used in the calculation
+        Returns
+        -------
+        snr : float
+             The Signal-to-Noise ratio of the given data
+        rms_bounds : tuple
+             The RMSs of the peak and noise arrays, (noise_rms, peak_rms)
+        peak_peak : float
+             The absolute difference between the RMSs of the peak and noise arrays
+        ratio_noise : float
+             The ratio between the ranges of noise and peak value(s)
+        sd_noise : float
+             The standard deviation of the noise values
+        ir_noise : ndarray
+             The indexes of noise values used in the calculation
+        ir_peak : int
+             The index of peak values used in the calculation
 
         Notes
         -----
-        Must be applied to signals with upward deflections (Peak > noise).
-        Assumes max noise value < (peak / 5)
-        Auto-detects noise section as the last noise_count values before the final noisy peak.
-       """
+            Must be applied to signals with upward deflections (Peak > noise).
+            Assumes max noise value < (peak / 5)
+            Auto-detects noise section as the last noise_count values before the final noisy peak.
+        """
     # Check parameters
     if type(signal_in) is not np.ndarray:
         raise TypeError('Signal data type must be an "ndarray"')
@@ -272,16 +272,16 @@ def snr_map(stack_in):
     """Generate a map of Signal-to-Noise ratios for signal arrays within a stack,
     defined as the ratio of the Peak-Peak amplitude to the population standard deviation of the noise.
 
-       Parameters
-       ----------
-       stack_in : ndarray
-           A 3-D array (T, Y, X) of optical data
+        Parameters
+        ----------
+        stack_in : ndarray
+            A 3-D array (T, Y, X) of optical data
 
-       Returns
-       -------
-       map : ndarray
-            A 2-D array of Signal-to-Noise ratios
-       """
+        Returns
+        -------
+        map : ndarray
+             A 2-D array of Signal-to-Noise ratios
+        """
     # Check parameters
     pass
 
@@ -290,20 +290,22 @@ def calculate_error(ideal, modified):
     """Calculate the amount of error created by signal modulation or filtering,
     defined as (Ideal - Modified) / Ideal X 100%.
 
-       Parameters
-       ----------
-       ideal : ndarray
-            An array of ideal data
-       modified : ndarray
-            An array of modified data
+        Parameters
+        ----------
+        ideal : ndarray
+             An array of ideal data
+        modified : ndarray
+             An array of modified data
 
-       Returns
-       -------
-       error : ndarray
-            An array of percent error
-       error_mean : float
-            The mean value of the percent error array
-       error_sd : float
-            The standard deviation of the percent error array
-       """
+        Returns
+        -------
+        error : ndarray
+             An array of percent error
+        error_mean : float
+             The mean value of the percent error array
+        error_sd : float
+             The standard deviation of the percent error array
+        """
     # Check parameters
+
+    pass
